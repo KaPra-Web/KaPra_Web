e33892ba694ac181455499b0942c7a7b61a79b5e
karanpopat <karanpopat@gmail.com>
2018-10-07 23:27:15 +0530

    sprin boot project
    sprin boot project

---
+++ KaPra/src/main/java/com/kapra/web/KaPraApplication.java
@@ -0,0 +1,12 @@
+package com.kapra.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaPraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaPraApplication.class, args);
+	}
+}
